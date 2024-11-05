e8c978a1d22fd01ed3f266587e8c2815b93c792e
Hoang Nguyen <freddyfrank69@gmail.com>
2024-11-05 09:09:50 +0700

    Bg fix

--- app/(public)/login.tsx
+++ app/(public)/login.tsx
@@ -157,6 +157,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    width: '100%',
   },
   logoContainer: {
     position: 'absolute',
